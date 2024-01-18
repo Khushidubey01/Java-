b43b96c6fca8863ab4470eff7b1ecb02f89a415a
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-18 22:02:24 +0530

    Add files via upload

---
+++ pt.java
@@ -0,0 +1,19 @@
+
+public class pt {
+    public static void main(String[] args) {
+        System.out.println("Here your pattern:\n");
+       int a=1;
+        
+        do{
+            int b=5;
+            do{
+                System.out.print("*");
+                b--;
+            }
+            while(b>=a);
+            System.out.print("\n");
+            a++;}      
+        while(a<=5);
+    }      
+    
+}
