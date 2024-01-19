7356958cbb0089e4bb648a5e8566e2c6a2db53bb
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-19 13:14:03 +0530

    Add files via upload

---
+++ conti_.java
@@ -0,0 +1,14 @@
+
+public class conti_ {
+      public static void main(String arg[])
+    {
+        int i=0;
+        while(i<=50)
+        {i++;
+        if(i%10==0)
+        {continue;}
+            System.out.print(i+" ");}
+            
+            }}
+        
+    
