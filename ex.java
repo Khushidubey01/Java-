5ba7a0791793e264b2a813b41d52a09173dd648f
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-19 12:27:14 +0530

    Add files via upload

---
+++ ex.java
@@ -0,0 +1,16 @@
+
+public class ex {
+    public static void main(String arg[])
+    {
+        int i,j;
+        for(i=1;i<=5;i++)
+        {for(j=1;j<=5;j++)
+            {if(i==j)
+            {continue;}
+            
+            {
+                System.out.print("\n"+i+" "+j);
+            }}
+        }
+    }
+}
