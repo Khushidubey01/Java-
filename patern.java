930e3dfca25577dc73c15b3c9c7b46e9b656f1d5
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-23 20:16:01 +0530

    Add files via upload

---
+++ patern.java
@@ -0,0 +1,18 @@
+
+public class patern {
+    public static void main(String arg[])
+    {
+        System.out.println("Here your pattern:");
+        int i,j,row=1;
+        for(i=5;i>=row;i--)
+        {
+            for(j=row+i;j<=5;j++)
+            {
+              System.out.print(" ");}
+            for(j=1;j<=i;j++) 
+            {System.out.print("*");}
+        System.out.print("\n");}
+        }
+        
+    }
+    
